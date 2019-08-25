9cdf963aa09177f8cb8f4602920465c282d2dad2
ubunturifa <jstrfaheem065@gmail.com>
2019-08-25 21:30:06 +0530

    Added home page and main class,

---
+++ calculatorapp/lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override 
+  State createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+  var numOne = 0;
+  var numTwo = 0;
+  var sum = 0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }}
