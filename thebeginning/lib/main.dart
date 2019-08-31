122b75455858fa4bc1d6c0d6114bcc8c098c292c
ubunturifa <jstrfaheem065@gmail.com>
2019-08-31 15:22:57 +0530

    Beginning tutorial,

---
+++ thebeginning/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
