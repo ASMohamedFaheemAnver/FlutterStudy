30dfd98c29ea2606b44d9e4b6e43c907b3591ed5
ubunturifa <jstrfaheem065@gmail.com>
2019-09-01 21:25:36 +0530

    Stateless and Stateful widget,

--- thebeginning/lib/main.dart
+++ thebeginning/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,12 +8,28 @@ class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'The Beginning',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        appBar: AppBar(
+          title: Text('Random words!'),
+        ),
+        body: Center( 
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords>{
+  @override 
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
