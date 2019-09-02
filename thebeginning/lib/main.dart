b99ea3f390db666ea7b090bca398b59890f7a791
ubunturifa <jstrfaheem065@gmail.com>
2019-09-03 00:50:01 +0530

    Tappable icon and favorites page,

--- thebeginning/lib/main.dart
+++ thebeginning/lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'The Beginning',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
@@ -21,8 +24,55 @@ class RandomWords extends StatefulWidget{
 
 class RandomWordsState extends State<RandomWords>{
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: _buildSuggestions(),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair){
+              return ListTile( 
+                title: Text( 
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+          );
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(),
+            body: ListView( 
+              children: divided,
+            ),
+          );
+        }
+      ),
+    );
+  }
+
   Widget _buildSuggestions(){
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -40,18 +90,25 @@ class RandomWordsState extends State<RandomWords>{
   }
 
   Widget _buildRow(WordPair pair){
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text( 
         pair.asPascalCase,
         style: _biggerFont,
       ),
-    );
-  }
-
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: _buildSuggestions(),
+      trailing: Icon( 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
