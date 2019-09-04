15761a24d410e838ed8a1d36751702cb2aec4a0f
ubunturifa <jstrfaheem065@gmail.com>
2019-09-05 02:36:09 +0530

    # https://flutter.dev/docs/development/ui/widgets-intro#hello-world

--- introtowidgets/lib/main.dart
+++ introtowidgets/lib/main.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    Center(
-      child: Text(
-        "Hello, world!",
-        textDirection: TextDirection.ltr,
+  runApp(new MaterialApp(
+    title: 'My App',
+    home: MyScaffold(),
+  ));
+}
+
+class MyScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example Title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+        ],
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget{
+  final Widget title;
+  MyAppBar({this.title});
+
+  @override 
+  Widget build(BuildContext context){
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation Menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
 }
