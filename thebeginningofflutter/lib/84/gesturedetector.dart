9360109f49d3cc7e7e049802eb3052a04c3661c5
ubunturifa <jstrfaheem065@gmail.com>
2019-11-05 19:42:30 +0530

    GestureDetector and snackBar fixed,

--- thebeginningofflutter/lib/84/gesturedetector.dart
+++ thebeginningofflutter/lib/84/gesturedetector.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(new HomePage());
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
       title: "THE GESTURE DETECTOR",
       home: new Scaffold(
-        body: new Container(
-          child: new GestureDetector(
-            onTap: (){
-              final snackBar = new SnackBar(content: new Text("GESTURE!"));
-              Scaffold.of(context).showSnackBar(snackBar);
-            },
+        // https://stackoverflow.com/questions/51304568/scaffold-of-called-with-a-context-that-does-not-contain-a-scaffold
+        body: new Builder(
+          builder: (context) => Center(
+            child: new GestureDetector(
+              onTap: (){
+                final snackBar = new SnackBar(content: new Text("GESTURE!"));
+                Scaffold.of(context).showSnackBar(snackBar);
+              },
+
+              // The actual button!
+              child: new Container(
+                padding: new EdgeInsets.all(18.0),
+                decoration: new BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: new BorderRadius.circular(5.5)
+                ),
+                child: new Text("FIRST BUTTON!"),
+              ),
+            ),
           ),
         ),
       ),
-    )
-  );
+    );
+  }
 }
