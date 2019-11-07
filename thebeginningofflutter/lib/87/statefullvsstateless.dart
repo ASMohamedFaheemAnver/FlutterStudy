9222131f9356411a32a00b61f7621b0db8961509
ubunturifa <jstrfaheem065@gmail.com>
2019-11-07 13:41:16 +0530

    Stateful widget and stateless widget,

--- thebeginningofflutter/lib/87/statefullvsstateless.dart
+++ thebeginningofflutter/lib/87/statefullvsstateless.dart
@@ -8,7 +8,12 @@ void main(){
   ));
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
