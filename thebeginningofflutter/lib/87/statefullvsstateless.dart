9c1d3a08a788935ff82fda1d736da889870ebce5
ubunturifa <jstrfaheem065@gmail.com>
2019-11-07 13:19:40 +0530

    Stateful widget and stateless widget,

---
+++ thebeginningofflutter/lib/87/statefullvsstateless.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: 'STATE OF APPLICATION',
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: new Builder(
+          builder: (context) => Center(
+            child: new GestureDetector(
+              onTap: (){
+                final snackbar = new SnackBar(content: new Text("THE GESTURE!"));
+                Scaffold.of(context).showSnackBar(snackbar);
+              },
+
+              child: new Container(
+                padding: EdgeInsets.all(19.0),
+                decoration: new BoxDecoration(
+                    color: Colors.lightBlue
+                ),
+                child: new Text("BUTTON!"),
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
