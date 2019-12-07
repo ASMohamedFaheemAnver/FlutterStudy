286340708a286af8aefe7ad3004e6d79b4876af4
ubunturifa <jstrfaheem065@gmail.com>
2019-12-07 23:11:58 +0530

    Stateful state,

--- thebeginningofflutter/lib/87/statefullvsstateless.dart
+++ thebeginningofflutter/lib/87/statefullvsstateless.dart
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(
-    new MaterialApp(
-      title: 'STATE OF APPLICATION',
-      home: new Scaffold(
-        body: new Builder(
-          builder: (context) => Center(
-            child: new GestureDetector(
-              onTap: (){
-                final snackBar = new SnackBar(content: new Text("GESTURE!"));
-                Scaffold.of(context).showSnackBar(snackBar);
-              },
-
-              // The actual button!
-              child: new Container(
-                padding: new EdgeInsets.all(18.0),
-                decoration: new BoxDecoration(
-                    color: Theme.of(context).accentColor,
-                    borderRadius: new BorderRadius.circular(5.5)
-                ),
-                child: new Text("FIRST BUTTON!"),
-              ),
-            ),
-          ),
-        ),
-      ),
-    )
-  );
-=======
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -101,5 +69,4 @@ class HomePageState extends State<HomePage>{
       ),
     );
   }
->>>>>>> origin/master
 }
