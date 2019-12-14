f61fdb8fae7be30ef558c9e7278070b0607ae34c
ubunturifa <jstrfaheem065@gmail.com>
2019-12-14 11:20:14 +0530

    Raised button with direction specification!,

--- first_lecture/lib/scratch/first_widget.dart
+++ first_lecture/lib/scratch/first_widget.dart
@@ -6,19 +6,56 @@ class KelaniyaApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Will align to the given parent
-    return new Center(
-        child: Material(
-            child: new Directionality(
-                textDirection: TextDirection.ltr,
-                child: RaisedButton(
-                  onPressed: () => {
-                  },
-                  child: new Text(
-                    "Hello From Raised Button!",
-                  style: TextStyle(color: Colors.red),),
-                )
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: new Text(("HELLO FROM FLUTTER!")),
+        ),
+        // Size box enable us to use cross axis alignment!
+        body: Builder(
+          builder: (context)=> SizedBox.expand(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+          // Vertical alignment
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: new Text("YOU CLICKED ME!"),
+                ));
+              },
+              child: Text("HELLO FROM FLUTTER!"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: new Text("YOU CLICKED ME!"),
+                ));
+              },
+              child: Text("HELLO FROM FLUTTER!"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: new Text("YOU CLICKED ME!"),
+                ));
+              },
+              child: Text("HELLO FROM FLUTTER!"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: new Text("YOU CLICKED ME!"),
+                ));
+              },
+              child: Text("HELLO FROM FLUTTER!"),
             )
+          ],
+        ),
+      ),
         )
+      ),
     );
   }
 }
