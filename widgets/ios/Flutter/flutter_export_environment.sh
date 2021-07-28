dd725e543d9583568c1ba0288b8cdf59a9b92e1e
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-07-28 07:27:09 +0530

    Pub get,

--- widgets/ios/Flutter/flutter_export_environment.sh
+++ widgets/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ubunturifa/Desktop/GitHub/FlutterStudy3/introtowidgets"
+export "FLUTTER_ROOT=/home/freedom/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/freedom/Desktop/GitHub/FlutterStudy/widgets"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/opt/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
