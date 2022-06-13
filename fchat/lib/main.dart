185732eec80ce62baa6cd0bb644f3a3319cd30b7
FreeDoM <jstrfaheem065@gmail.com>
2022-06-13 22:54:23 +0530

    Chatting starter,

---
+++ fchat/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Text(
+        "Facebook",
+        style: TextStyle(color: Colors.black),
+      ),
+    ),
+  ));
+}
