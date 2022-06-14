8e5c6c136b04b8e4607fc43f8c9e9026d1499a70
FreeDoM <jstrfaheem065@gmail.com>
2022-06-14 20:44:21 +0530

    Firestore integration,

--- fchat/lib/main.dart
+++ fchat/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -23,25 +26,52 @@ class BasicChat extends StatefulWidget {
 }
 
 class BasicChatState extends State<BasicChat> {
-  late ChatUser currentUser;
-  late List<ChatMessage> messages;
+  ChatUser currentUser = ChatUser(id: "144", customProperties: {"msg": ""});
+  ChatUser otherUser = ChatUser(id: "104", customProperties: {"msg": ""});
+  List<ChatMessage> messages = <ChatMessage>[];
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  late DocumentReference roomRef = db.collection("fchats").doc("104#144");
+  late CollectionReference messagesRef = roomRef.collection("messages");
 
   @override
   void initState() {
     super.initState();
-    currentUser = ChatUser(id: "144");
-    messages = <ChatMessage>[];
+    roomRef.get().then((value) {
+      if (!value.exists) {
+        roomRef.set({
+          "users": [currentUser.toJson(), otherUser.toJson()]
+        });
+      }
+    });
+    messagesRef
+        .orderBy("createdAt", descending: true)
+        .snapshots()
+        .listen((event) {
+      setState(() {
+        messages = event.docs
+            .where((message) => message.data() != null)
+            .map((e) => ChatMessage.fromJson(e.data() as Map<String, dynamic>))
+            .toList();
+        inspect(messages);
+        // messages = List<dynamic>.from(event.docs.map((e) => e.data()))
+        //     .map((e) => ChatMessage.fromJson(e))
+        //     .toList();
+        // messages = event.docs
+        //     .map((e) => ChatMessage.fromJson(e.data()))
+        //     .toList();
+      });
+    }).onError((err) => {inspect(err)});
   }
 
   @override
   Widget build(BuildContext context) {
+    // inspect(messages);
     return Scaffold(
       body: DashChat(
           currentUser: currentUser,
           onSend: (ChatMessage message) {
-            setState(() {
-              messages.insert(0, message);
-            });
+            message.customProperties = {"msg": ""};
+            messagesRef.add(message.toJson());
           },
           messages: messages),
     );
