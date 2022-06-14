154014b6b0ad59dffb68d41e20fa77e210a02391
FreeDoM <jstrfaheem065@gmail.com>
2022-06-14 13:59:06 +0530

    Basic chat functionality added,

--- fchat/lib/main.dart
+++ fchat/lib/main.dart
@@ -1,12 +1,42 @@
+import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Scaffold(
-      body: Text(
-        "Facebook",
-        style: TextStyle(color: Colors.black),
-      ),
-    ),
+    home: BasicChat(),
   ));
+}
+
+class BasicChat extends StatefulWidget {
+  const BasicChat({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return BasicChatState();
+  }
+}
+
+class BasicChatState extends State<BasicChat> {
+  late ChatUser currentUser;
+  late List<ChatMessage> messages;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = ChatUser(id: "144");
+    messages = <ChatMessage>[];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DashChat(
+          currentUser: currentUser,
+          onSend: (ChatMessage message) {
+            setState(() {
+              messages.insert(0, message);
+            });
+          },
+          messages: messages),
+    );
+  }
 }
